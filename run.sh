ab580677b0ee3abb754bb3964f3ff78c57523439
motemen <motemen@gmail.com>
2014-11-08 14:25:36 +0900

    nytprof

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export TMPDIR=/dev/shm
-exec carton exec plackup -s Starlet \
+export NYTPROF=sigexit=int:savesrc=0:start=no
+exec carton exec perl -d:NYTProf local/bin/plackup -s Starlet \
     --max-reqs-per-child=50000 --min-reqs-per-child=50000 \
     --max-workers=5 \
     --host localhost:8080 -E prod app.psgi
