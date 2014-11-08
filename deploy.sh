402f02884e595218b50b45d4ad412e0b630a560d
motemen <motemen@gmail.com>
2014-11-08 12:23:23 +0900

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+echo '---> isu31a'
+ssh isucon@203.104.111.191 "cd ~/deploy && git pull && carton install --deployment"
+
+echo '---> isu31b'
+ssh isucon@203.104.111.192 "cd ~/deploy && git pull && carton install --deployment"
+
+echo '---> isu31c'
+ssh isucon@203.104.111.193 "cd ~/deploy && git pull && carton install --deployment"
