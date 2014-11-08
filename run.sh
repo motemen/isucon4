f25dae86ce529a2df31ab80bad7d2e8da4abb584
y_uuki <yuki.tsubo@gmail.com>
2014-11-08 18:00:01 +0900

    80 でうごかすように

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # export TMPDIR=/dev/shm
-exec carton exec plackup \
-    --port 8080 \
+exec su -c "carton exec plackup \
+    --port 80 \
     -s Starlet \
     --max-reqs-per-child=50000 --min-reqs-per-child=50000 \
     --max-workers=5 \
-    -E prod app.psgi
+    -E prod app.psgi"
