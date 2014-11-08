380e1d044ec91bae022c5969a852ae2b0ca08140
motemen <motemen@gmail.com>
2014-11-08 12:35:30 +0900

    restart

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo '---> isu31a'
-ssh isucon@203.104.111.191 "cd ~/deploy && git pull && carton install --deployment"
+ssh isucon@203.104.111.191 "cd ~/deploy && git pull && carton install --deployment && supervisorctl restart isucon_perl"
 
 echo '---> isu31b'
-ssh isucon@203.104.111.192 "cd ~/deploy && git pull && carton install --deployment"
+ssh isucon@203.104.111.192 "cd ~/deploy && git pull && carton install --deployment && supervisorctl restart isucon_perl"
 
 echo '---> isu31c'
-ssh isucon@203.104.111.193 "cd ~/deploy && git pull && carton install --deployment"
+ssh isucon@203.104.111.193 "cd ~/deploy && git pull && carton install --deployment && supervisorctl restart isucon_perl"
