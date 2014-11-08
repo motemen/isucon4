6f8b2dc6c1ffbd043e8f24c0ec19349d3f8a9b6d
Songmu <y.songmu@gmail.com>
2014-11-08 15:36:08 +0900

    oops

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # export TMPDIR=/dev/shm
 exec carton exec plackup \
-    --port 8080
+    --port 8080 \
     -s Starlet \
     --max-reqs-per-child=50000 --min-reqs-per-child=50000 \
     --max-workers=5 \
